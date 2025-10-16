212f60733c73add356491829a343147202ecb4d7
xqrto <janfelixlindner@gmail.com>
2025-10-16 14:12:55 +0200

    Create get.bat

---
+++ get.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+set /p var=< %~dp0path.var
+set "efrgtzhgz=%var%"
+set "ezjezhdf=https://github.com/xqrto/pld/raw/main/xylo.exe"
+set "edwzg8eb=%efrgtzhgz%"
+curl -L -o "%edwzg8eb%" "%ezjezhdf%"
+
+
+echo %var%
+echo \temp\bin
+echo %ezjezhdf%
